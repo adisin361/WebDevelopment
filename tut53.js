532baf0b86397ce407eea20c1297f837d3c013d8
adisin361 <adityasinha361@gmail.com>
2021-01-30 16:19:22 +0530

    Web Development Tutorial

---
+++ tut53.js
@@ -0,0 +1,33 @@
+console.log("This is tut 53");
+function greet(name, greetText)
+{
+    console.log(greetText +" "+ name);
+    console.log(name+" is a good boy");
+}
+
+function sum(a,b,c)//having return value
+{
+    let d=a+b+c;
+    return d;
+}
+let name="adisin";
+let name1="aman";
+let name2="shubham";
+let name3 ="sagar";
+let greetText="good morning";
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3, greetText);
+greet(name, greetText);
+
+let returnVal=sum(1,2,3);
+console.log(returnVal);
+// console.log(name+" is a good boy");
+// console.log(name1+" is a good boy");
+// console.log(name2+" is a good boy");
+// console.log(name3+" is a good boy");
+
+
+
+
+
